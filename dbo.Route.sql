16e7de79fd1df45a3052f5d056c8e7411a8f6a38
root <root@localhost.localdomain>
2019-05-25 22:01:15 +0300

    Забыл добавить RouterView

--- dbo.Route.sql
+++ dbo.Route.sql
@@ -1,8 +1,7 @@
-﻿CREATE VIEW [dbo].[RouteView]
-AS
-(
-	SELECT A.Id, A.Article, B.Name NameArticle , A.[Group], C.Name NameGroup
-	FROM Route A
-	LEFT JOIN dbo.Article B ON A.Article = B.Id
-	LEFT JOIN dbo.[Group] C ON A.[Group] = C.Id
-)
+﻿CREATE TABLE [dbo].[Route] (
+    [Id]      INT IDENTITY (1, 1) NOT NULL,
+    [Article] INT NULL,
+    [Group]   INT NULL
+);
+
+
---
+++ dbo.RouteView.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[RouteView]
+AS
+(
+	SELECT A.Id, A.Article, B.Name NameArticle , A.[Group], C.Name NameGroup
+	FROM Route A
+	LEFT JOIN dbo.Article B ON A.Article = B.Id
+	LEFT JOIN dbo.[Group] C ON A.[Group] = C.Id
+)
